a1b98347b37dfb7fb86095208b556e4d2c0e245a
aknokhwal <aknokhwal01@gmail.com>
2024-04-24 17:29:08 +0530

    tour

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useState} from "react";
+import tourData from "./tourData";
 
-function App() {
+const Tour = () => {
+    const [travelData, setTravelData] = useState(tourData);
+    const handleEvent = ( id) => {
+        setTravelData(
+            travelData.filter(
+                (item) => id != item.id
+            )
+        )
+    }
+
+    return(
+        <div className="container">
+            {travelData.map(({ id, tourDestination, description, imageUrl }) => (
+                <div key={id} className="card m-4" style={{width: "18rem"}}>
+                    <img src={imageUrl} className="card-img-top" alt={tourDestination} />
+                    <div className="card-body">
+                        <h5 className="card-title">{tourDestination}</h5>
+                        <p className="card-text">{description}</p>
+                        <button className="btn btn-primary" onClick={() => handleEvent(id)}>Not Interested</button>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App mt-5'>
+      <Tour />
     </div>
-  );
+  )
 }
 
 export default App;
